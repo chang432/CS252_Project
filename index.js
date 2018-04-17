06c14a499440c53bc5dfbdd191f40926de804c3f
andre <chang432@purdue.edu>
2018-04-17 04:25:55 -0400

    created

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function login() {
-	window.alert("Working!");
-}
+var express = require('express')
+var app = express();
+var PORT = process.env.PORT || 5000
+
+app.get("/", function(req, res) {
+	res.send("I HATE MY LIFE");
+});
+
+app.listen(PORT);
