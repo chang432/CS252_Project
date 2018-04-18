e439004d64cee596c4a304b65fe5967cd2734c23
andre <chang432@purdue.edu>
2018-04-17 23:08:40 -0400

    edits

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var app = express();
 var PORT = process.env.PORT || 5000
 
 app.get("/", function(req, res) {
-	res.send("I HATE MY LIFE");
+	//res.send("I HATE MY LIFE");
+	res.sendFile(__dirname + '/client/index.html');
 });
 
 app.listen(PORT);
